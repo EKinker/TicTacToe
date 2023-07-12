138dbd9e3e8d78e383e476f2203132d50220bc51
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-12 13:00:44 -0400

    Methods Started

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,15 +4,26 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;  //declare character matrix
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        boolean result = false;
+        if (isRowWin('X') == true){
+            return true;
+        }
+        return result;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        boolean result = false;
+        if (isRowWin('O') == true){
+            return true;
+        }
+        return result;
     }
 
     public Boolean isTie() {
@@ -21,6 +32,16 @@ public class Board {
 
     public String getWinner() {
         return null;
+    }
+
+    public boolean isRowWin(char input) {
+        boolean result = false;
+        for (int i = 0; i<3; i++) {
+            if (matrix [i][0] == input && matrix[i][0] == matrix[i][1] && matrix[i][1]== matrix[i][2]) {
+                result = true;
+            }
+        }
+        return result;
     }
 
 }
